4c2c8528ecfc48278001faae3efb8d882f728065
SreemanthG <sreemanth2001@gmail.com>
2020-06-18 18:07:15 +0530

    added skills

--- index.js
+++ index.js
@@ -157,3 +157,17 @@ if(window.location.hash) {
   }, 1000, 'swing');
 }    
 
+//gallery
+
+$(function() {
+  var selectedClass = "";
+  $(".filter").click(function(){
+  selectedClass = $(this).attr("data-rel");
+  $("#gallery").fadeTo(100, 0.1);
+  $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+  setTimeout(function() {
+  $("."+selectedClass).fadeIn().addClass('animation');
+  $("#gallery").fadeTo(300, 1);
+  }, 300);
+  });
+  });
